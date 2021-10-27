f8d5f72ce779ebac7610d2751a768406214e693c
Rajesh2795 <diddirajesh27@gmail.com>
2021-10-26 19:56:50 -0700

    Rectified Port

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname + "/public/index.html"));
 })
 
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log("Server is running on port: " + port);
